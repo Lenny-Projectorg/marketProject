3680619acb6d50f351623ed6c11b4eaa91af2b9c
Lenny <2029194345@qq.com>
2023-04-18 09:23:47 +0800

    初始化项目

--- market/src/main/java/org/example/market/test.java
+++ market/src/main/java/org/example/market/test.java
@@ -3,9 +3,5 @@ package org.example.market;
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("dsaf");
-        System.out.println("sdsadsadsadsaf");
-        System.out.println("sdwdjfhfqihbaf");
-        System.out.println("AAAAAA");
     }
 }
