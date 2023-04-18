80bd1e1045f0606cca0f25d6e13ab00024018c4e
Lenny <2029194345@qq.com>
2023-04-18 12:03:28 +0800

    初始化操作数据库

--- market/src/main/java/org/example/market/SpringApp.java
+++ market/src/main/java/org/example/market/SpringApp.java
@@ -1,9 +1,11 @@
 package org.example.market;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //用于启动SpringBoot项目的类
 @SpringBootApplication
+@MapperScan("org.example.market.dao")//表示扫描dao包下的注解
 public class SpringApp {
     public static void main(String[] args) {
         SpringApplication.run(SpringApp.class);
